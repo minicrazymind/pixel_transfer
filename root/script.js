065587adea8a824c82f04d5ca2e48a2e5686ebaf
Eric <60669258+minicrazymind@users.noreply.github.com>
2020-12-01 21:17:52 -0800

    Add files via upload

---
+++ root/script.js
@@ -0,0 +1,39 @@
+// This uses a pre-trained model of The Great Wave off Kanagawa and Udnie (Young American Girl, The Dance)
+let inputImg;
+let statusMsg;
+let transferBtn;
+let style1;
+
+
+function setup() {
+  noCanvas();
+  // Status Msg
+  statusMsg = select('#statusMsg');
+
+  // Get the input image
+  inputImg = select('#inputImg');
+
+  // Transfer Button
+  transferBtn = select('#transferBtn')
+  transferBtn.mousePressed(transferImages);
+
+  // Create two Style methods with different pre-trained models
+  style1 = ml5.styleTransfer('/models/pixels', modelLoaded);
+}
+// A function to be called when the models have loaded
+function modelLoaded() {
+  // Check if both models are loaded
+  if(style1.ready){
+    statusMsg.html('Ready!')
+  }
+}
+// Apply the transfer to both images!
+function transferImages() {
+  statusMsg.html('Applying Style Transfer...!');
+
+  style1.transfer(inputImg, function(err, result) {
+    createImg(result.src).parent('styleA');
+  });
+
+  statusMsg.html('Done!');
+}
